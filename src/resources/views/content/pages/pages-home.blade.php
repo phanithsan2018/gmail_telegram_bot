382b25363b3ec8e2635aec9c78e6dd33276cecb8
phanithsan2018 <165936577+phanithsan2018@users.noreply.github.com>
2024-09-11 02:43:30 +0700

    Test update from from local

--- src/resources/views/content/pages/pages-home.blade.php
+++ src/resources/views/content/pages/pages-home.blade.php
@@ -7,6 +7,6 @@ $configData = Helper::appClasses();
 @section('title', 'Home')
 
 @section('content')
-<h4>Home Page</h4>
+<h4>Tesing git with server by update code</h4>
 <p>For more layout options refer <a href="{{ config('variables.documentation') ? config('variables.documentation').'/laravel-introduction.html' : '#' }}" target="_blank" rel="noopener noreferrer">documentation</a>.</p>
 @endsection
